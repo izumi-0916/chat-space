6e278e33109dcc33a8ce77973acd13a759912a1f
izumi <kizumi916@gmail.com>
2020-02-13 17:27:51 +0900

    アラートを出す

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -82,7 +82,7 @@ var reloadMessages = function() {
     }
   })
   .fail(function() {
-    console.log('error');
+    alert("自動更新に失敗しました");
   });
 };
 if (document.location.href.match(/\/groups\/\d+\/messages/)) {
